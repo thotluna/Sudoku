02ebbf164039de0fd33542faf70c2ee1a187f377
thotluna <eladi.feijoo@gmail.com>
2024-07-03 09:30:18 -0400

    create session test

--- src/test/java/models/SessionTest.java
+++ src/test/java/models/SessionTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.*;
 
 class SessionTest {
 
@@ -15,14 +15,33 @@ class SessionTest {
     @BeforeEach
     void setUp() {
         game = new Game();
+        game.addCells(BoardSupport.getBoardSet().initial());
+        game.setSolution(BoardSupport.getBoardSet().solution());
         session = new Session(game);
     }
 
     @Test
     void GiveStartGame_WhenSessionCreated_ThenStateInitAndGameBoarBlank(){
+        Game game = new Game();
+        Session session = new Session(game);
+
         assertThat(session.getStateValue(), is(StateValue.INITIAL));
         assertThat(session.hasGame(), is(false));
-        assertThat(session.getBoard().get(), is(new ContainsValidCellsMatcher()));
+    }
+
+    @Test
+    void GiveAnOngoingGame_WhenSessionRestartCalled_ThenReloadBoardAndState(){
+        Coordinate coordinate = new Coordinate("A1");
+        game.getBoard().addCell(Cell.newCellCandidate(coordinate.getRow(), coordinate.getColumn(), 9));
+        Board boardOngoingGame = game.getBoard().newCopy();
+
+        session.restart();
+
+        assertThat(game.hasGame(), is(true));
+        assertThat(game.getBoard(), not(equalTo(boardOngoingGame)));
+        assertThat(game.getBoard().isNullCell(coordinate), is(true));
+        assertThat(session.getStateValue(), is(StateValue.IN_GAME));
+
     }
 
 }
